59ea2f7fbd11d9e896511fbf1afb5a727ac09961
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-06-21 16:40:20 +0300

    fix: minor change

--- destructuring.js
+++ destructuring.js
@@ -28,3 +28,44 @@ function calculate(a, b) {
 }
 
 const [add, subtract, multiply, divide] = calculate(4, 7);
+
+
+
+// destructuring in function using object
+const vehicleOne = {
+  brand: 'Ford',
+  model: 'Mustang',
+  type: 'car',
+  year: 2021, 
+  color: 'red'
+}
+
+myVehicle(vehicleOne);
+
+function myVehicle({type, color, brand, model}) {
+  const message = 'My ' + type + ' is a ' + color + ' ' + brand + ' ' + model + '.';
+}
+
+
+
+
+
+// destructure deeply nested objects by referencing the nested object then using a colon and curly braces to again destructure the items needed from the nested object:
+const vehicleTwo = {
+  brand: 'Ford',
+  model: 'Mustang',
+  type: 'car',
+  year: 2021, 
+  color: 'red',
+  registration: {
+    city: 'Houston',
+    state: 'Texas',
+    country: 'USA'
+  }
+}
+
+myVehicle(vehicleTwo)
+
+function myVehicle({ model, registration: { state } }) {
+  const message = 'My ' + model + ' is registered in ' + state + '.';
+}
