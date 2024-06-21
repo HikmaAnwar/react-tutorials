9b1c1616767d72e0b886a48b1d222fc23720ac7e
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-06-21 15:43:17 +0300

    feat:created an arrow function

---
+++ arrow_function.js
@@ -0,0 +1,9 @@
+// Arrow Functions Return Value by Default:
+hello = () => "hello world!";
+
+// Arrow Function With Parameters:
+hello = (val) => "hello" + val;
+
+// without parentesis 
+hello = val => "hello" + val;
+
