be88032cbc2931d59fb786b13a8c3cd4568c64e8
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-06-21 15:51:03 +0300

    fix: minor change

--- arrow_function.js
+++ arrow_function.js
@@ -7,3 +7,43 @@ hello = (val) => "hello" + val;
 // without parentesis 
 hello = val => "hello" + val;
 
+// With a regular function, this represents the object that called the function:
+class Header{
+    constructor(){
+        this.color = "red";
+    }
+    //Regular function:
+  changeColor = function() {
+    document.getElementById("demo").innerHTML += this;
+  }
+}
+const myheader = new Header();
+//The window object calls the function:
+window.addEventListener("load", myheader.changeColor);
+
+//A button object calls the function:
+document.getElementById("btn").addEventListener("click", myheader.changeColor);
+
+
+
+
+// With an arrow function, this represents the Header object no matter who called the function:
+class Headerr {
+    constructor() {
+      this.color = "Red";
+    }
+  
+  //Arrow function:
+    changeColorr = () => {
+      document.getElementById("demo").innerHTML += this;
+    }
+  }
+  
+  const myheaderr = new Header();
+  
+  
+  //The window object calls the function:
+  window.addEventListener("load", myheaderr.changeColor);
+  
+  //A button object calls the function:
+  document.getElementById("btn").addEventListener("click", myheaderr.changeColor);
